382898a25276c7ca6a990938b9ac886dfbbb0839
Eduard Maura <edu.maura.puig@gmail.com>
2017-09-06 19:52:26 +0200

    Add initial orientation test
    Rover is only modified to compile

--- src/test/java/org/barcelonasoftwarecraftmanship/marsoverkata/RoverTest.java
+++ src/test/java/org/barcelonasoftwarecraftmanship/marsoverkata/RoverTest.java
@@ -12,4 +12,10 @@ public class RoverTest {
         assertThat(rover.getX(), is(3));
         assertThat(rover.getY(), is(4));
     }
+
+    @Test
+    public void givenAnInitialOrientation_whenAskForCurrentOrientation_thenRoverReturnsInitialOrientation() throws Exception {
+        Rover rover = new Rover(Orientation.NORTH);
+        assertThat(rover.getCurrentOrientation(), is(Orientation.NORTH));
+    }
 }
