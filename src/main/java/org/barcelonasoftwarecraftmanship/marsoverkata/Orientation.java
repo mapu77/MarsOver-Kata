ba4d1af615d426cab5ee82e71c4bc810bdcd4e3c
Eduard Maura <edu.maura.puig@gmail.com>
2017-09-06 20:08:35 +0200

    Add Orientation enum
    Sorry for not committing it before

---
+++ src/main/java/org/barcelonasoftwarecraftmanship/marsoverkata/Orientation.java
@@ -0,0 +1,11 @@
+package org.barcelonasoftwarecraftmanship.marsoverkata;
+
+/**
+ * Created by Eduard Maura on 4/09/17
+ */
+public enum Orientation {
+    NORTH,
+    EAST,
+    SOUTH,
+    WEST
+}
