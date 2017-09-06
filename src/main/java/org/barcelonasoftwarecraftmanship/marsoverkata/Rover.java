cf469aaedd823c47c056f7561395cb71bccab3bc
Eduard Maura <edu.maura.puig@gmail.com>
2017-09-06 19:46:57 +0200

    Add initial position test case
    Create Rover class only to compile and make test go red

---
+++ src/main/java/org/barcelonasoftwarecraftmanship/marsoverkata/Rover.java
@@ -0,0 +1,18 @@
+package org.barcelonasoftwarecraftmanship.marsoverkata;
+
+public class Rover {
+    private Integer x;
+    private Integer y;
+
+    public Rover(int x, int y) {
+
+    }
+
+    public Integer getX() {
+        return x;
+    }
+
+    public Integer getY() {
+        return y;
+    }
+}
---
+++ src/test/java/org/barcelonasoftwarecraftmanship/marsoverkata/RoverTest.java
@@ -0,0 +1,15 @@
+package org.barcelonasoftwarecraftmanship.marsoverkata;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class RoverTest {
+    @Test
+    public void givenAnInitialPoint_whenAskForCurrentPosition_thenRoverReturnsTheInitialPosition() throws Exception {
+        Rover rover = new Rover(3,4);
+        assertThat(rover.getX(), is(3));
+        assertThat(rover.getY(), is(3));
+    }
+}
